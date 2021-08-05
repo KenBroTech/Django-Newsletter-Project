e34111aebe00e184c6456323621bbd1f88d1bfe5
KenBroTech <80349696+KenBroTech@users.noreply.github.com>
2021-08-05 10:10:02 +0100

    Update settings.py

--- newsletter/settings.py
+++ newsletter/settings.py
@@ -126,5 +126,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'youremail@gmail.com'
-EMAIL_HOST_PASSWORD = 'youremailpassword'
+EMAIL_HOST_USER = 'youremail@gmail.com' #Enter your email
+EMAIL_HOST_PASSWORD = 'youremailpassword' #Enter the password to the email
